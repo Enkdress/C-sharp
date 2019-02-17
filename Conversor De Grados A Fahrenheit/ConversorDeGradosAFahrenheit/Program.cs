0ed75dc486f27836947f14939687037e0ecefb06
Enkdress <40218829+Enkdress@users.noreply.github.com>
2019-02-17 01:22:07 -0500

    Add files via upload
    Conversor

---
+++ Conversor De Grados A Fahrenheit/ConversorDeGradosAFahrenheit/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversorDeGradosAFahrenheit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string celsius;
+            float grados;
+            float fahrenheit;
+
+            //Pedimos los grados a convertir
+            Console.WriteLine("Grados en Celsius: ");
+            celsius = Console.ReadLine();
+
+            //lo convertimos a float
+            grados = Convert.ToSingle(celsius);
+
+            //calculamos el valor de grados en fahrenheit
+            fahrenheit = (grados * 9/5)+32;
+            
+            //mostramos en consola el valor de grados en fahrenheit
+            Console.WriteLine("Aqui tienes la respuesta:\n{0}°C son {1}°F",grados, fahrenheit);
+        }
+    }
+}
